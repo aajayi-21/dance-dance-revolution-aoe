1772e0496359d01013cddf7e08721afa9cbc7486
Adeolu Ajayi <adeolu.ajayi@protonmail.com>
2023-04-22 15:02:36 -0400

    Fixed Collision Detection problems. Added more of the song to the list. Added point tracking.

--- main.py
+++ main.py
@@ -46,9 +46,6 @@ right_arrow_surface = pygame.transform.scale(down_arrow_surface, (112.33, 107.66
 right_arrow_surface = pygame.transform.rotate(down_arrow_surface, 90)
 right_arrow_surface_rect = right_arrow_surface.get_rect(topleft=(768, 10))
 
-# create points surface
-points_surface = my_font.render("Points: placeholder", False, (0, 0, 0))
-
 '''
 This class defines the arrow object which are the moving arrows on the screen. Each arrow has an x position, y position,
 speed, and source image file. The class also defines to functions for arrows, 'spawn' which displays it on the screen,
@@ -74,22 +71,27 @@ class Arrow(pygame.sprite.Sprite):
         self.image = pygame.image.load("clear_image.png")
 
 
-start_time = pygame.time.get_ticks()
-pygame.mixer.music.play()
 # SONG. When what arrows should be spawned in ms. It takes 2366.66666... milliseconds to travel from bottom to arrow.
 '''
 Songs will be in a list form. It will be a list of tuples where the first element is the time the arrow should 
 be spawned in milliseconds, the second element is its type, the third is the actual arrow object. The arrows travel
 at the same speed so it is know how long it takes to reach the arrow indicator.
 '''
-#TODO create a real song.
-test_song = [(1500, 'u', Arrow(512, 720, 5, "up_arrow.png")), (3000, 'd', Arrow(640, 720, 5, "down_arrow.png")),
-             (5123, "l", Arrow(384, 720, 5, "left_arrow.png")), (6000, "r", Arrow(768, 720, 5, "right_arrow.png")),
-             (6000, 'u', Arrow(512, 720, 5, "up_arrow.png"))]
+##TODO put in all the notes of the song. Don't put two of the same next to each other
+test_song = [(0, 'u', Arrow(512, 720, 5, "up_arrow.png")), (1010, 'd', Arrow(640, 720, 5, "down_arrow.png")),
+             (1516, "l", Arrow(384, 720, 5, "left_arrow.png")), (2010, "r", Arrow(768, 720, 5, "right_arrow.png")),
+             (3341, 'u', Arrow(512, 720, 5, "up_arrow.png")), (3512, 'd', Arrow(640, 720, 5, "down_arrow.png")),
+             (3852, 'd', Arrow(640, 720, 5, "down_arrow.png")), (4025, 'u',Arrow(512, 720, 5, "up_arrow.png")),
+             (4342, 'l',Arrow(384, 720, 5, "left_arrow.png")), (4510, 'r', Arrow(768, 720, 5, "right_arrow.png")),
+             (4848, 'd',Arrow(640, 720, 5, "down_arrow.png")), (5002, 'u', Arrow(512, 720, 5, "up_arrow.png")),
+             (5505, 'r', Arrow(768, 720, 5, "right_arrow.png")), (6022, 'l',Arrow(384, 720, 5, "left_arrow.png"))]
+happening = []
+points = 0
 
 '''
 This while loop is the actual game. Each time it runs the game updates. 
 '''
+start_time = pygame.time.get_ticks()
 while True:
     '''
     This block enables the exit arrow in the window to work.
@@ -106,7 +108,6 @@ while True:
     screen.blit(down_arrow_surface, down_arrow_surface_rect)
     screen.blit(left_arrow_surface, left_arrow_surface_rect)
     screen.blit(right_arrow_surface, right_arrow_surface_rect)
-    screen.blit(points_surface, (1024, 10))
 
     ''''
     Main game logic. Each loop the elapsed time from the game start time is taken. A list called happening is created.
@@ -115,54 +116,113 @@ while True:
     all its contents' y coordinate;making it move. The last block of if statements check for collision using posiiton.
     '''
     elapsed_time = pygame.time.get_ticks() - start_time
-    # print(elapsed_time)
-    happening = []
-    for note in test_song:
+    points_surface = my_font.render(f"Points: {points}", False, (0, 0, 0))
+    screen.blit(points_surface, (1024, 10))
+
+    if elapsed_time > 2366.66667 and pygame.mixer.music.get_busy() == False:
+        pygame.mixer.music.play()
+
+    for note in test_song: #Possibly Condense
         if (note[0]) <= elapsed_time:
             if note[1] == 'u':
                 note[2].spawn()
                 happening.append(note)
-            # UP_arrow.spawn()
+                test_song.remove(note)
+
             if note[1] == 'd':
                 note[2].spawn()
                 happening.append(note)
-                # DOWN_arrow.spawn()
+                test_song.remove(note)
+
             if note[1] == 'l':
                 note[2].spawn()
                 happening.append(note)
-            # LEFT_arrow.spawn()
+                test_song.remove(note)
+
             if note[1] == 'r':
                 note[2].spawn()
                 happening.append(note)
-            # RIGHT_arrow.spawn()
+                test_song.remove(note)
+
 
     for note in happening:
         note[2].rect.y -= 5
         note[2].spawn()
-    # print(happening)
 
-    if happening:  # TODO button pressed are only working for up arrows after the first one. Must fix
-        pressed = (pygame.key.get_pressed(), happening[-1][2].rect.y)
-        if happening[-1][1] == 'u':
+    if happening:  # TODO Fix timing and points
+        pressed = (pygame.key.get_pressed(), happening[0][2].rect.y)
+
+        if happening[0][1] == 'u':
             if pressed[0][pygame.K_UP]:
-                if 20 > (pressed[1] - 10) > -10:
-                    happening[-1][2].delete()
+                if 20 > (pressed[1] - 10) > -20:
+                    happening[0][2].delete()
+                    happening.pop(0)
                     print("Good!")
-        if happening[-1][1] == 'd':
+                    points += 10
+                else:
+                    happening[0][2].delete()
+                    happening.pop(0)
+                    print("Miss!")
+                    points -= 10
+
+            if pressed[1] < -15:
+                happening[0][2].delete()
+                happening.pop(0)
+                print("Miss!")
+                points -= 10
+                continue
+    if happening:
+        if happening[0][1] == 'd':
             if pressed[0][pygame.K_DOWN]:
-                if 20 > (pressed[1] - 10) > -10:
-                    happening[-1][2].delete()
+                if 20 > (pressed[1] - 10) > -20:
+                    happening[0][2].delete()
+                    happening.pop(0)
                     print("Good!")
-        if happening[-1][1] == 'l':
+                    points += 10
+                else:
+                    happening[0][2].delete()
+                    happening.pop(0)
+                    print("Miss!")
+                    points -= 10
+
+            if pressed[1] < -20:
+                happening[0][2].delete()
+                happening.pop(0)
+                print("Miss!")
+                points -= 10
+                continue
+    if happening:
+        if happening[0][1] == 'l':
             if pressed[0][pygame.K_LEFT]:
-                if 20 > (pressed[1] - 10) > -10:
-                    happening[-1][2].delete()
+                if 20 > (pressed[1] - 10) > -20:
+                    happening[0][2].delete()
+                    happening.pop(0)
                     print("Good!")
-        if happening[-1][1] == 'r':
+                    points += 10
+
+            if pressed[1] < -15:
+                happening[0][2].delete()
+                happening.pop(0)
+                print("Miss!")
+                points -= 10
+    if happening:
+        if happening[0][1] == 'r':
             if pressed[0][pygame.K_RIGHT]:
-                if 20 > (pressed[1] - 10) > -10:
-                    happening[-1][2].delete()
+                if 20 > (pressed[1] - 10) > -20:
+                    happening[0][2].delete()
+                    happening.pop(0)
                     print("Good!")
+                    points += 10
+
+            if pressed[1] < -15:
+                happening[0][2].delete()
+                happening.pop(0)
+                print("Miss!")
+                points -= 10
+                continue
+
+    if happening == False and elapsed_time > start_time:
+        print("Game Over!")
 
     pygame.display.update()
     clock.tick(60)  # The while true loops should not run faster than 60 times per second
